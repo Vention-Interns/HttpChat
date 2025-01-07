9cb58719900c2a954dd058fe7218aee6be013b4b
Vlada Gromova <Vlada.Gromova@ventionteams.com>
2025-01-07 17:24:22 +0100

    [VG]#12 - some tests for MessageService added

--- HttpChat/HttpChat/Controllers/AuthController.cs
+++ HttpChat/HttpChat/Controllers/AuthController.cs
@@ -7,25 +7,14 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
-namespace HttpChat.Controller
+namespace HttpChat.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IAuthService authService) : ControllerBase
     {
-        private readonly UserManager<UserModel> _userManager;
-        private readonly SignInManager<UserModel> _signInManager;
-        private readonly string _jwtSecretKey;
-
-        public AuthController(UserManager<UserModel> userManager, SignInManager<UserModel> signInManager, IConfiguration configuration)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _jwtSecretKey = configuration["AppSettings:JwtSecretKey"];
-        }
-
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginModelDto model)
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto dto)
         {
             if (!ModelState.IsValid)
             {
@@ -35,41 +24,18 @@ namespace HttpChat.Controller
                 });
             }
 
-            var user = await _userManager.FindByEmailAsync(model.Email);
+            var result = await authService.LoginAsync(dto);
 
-            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            if (result.IsSuccess)
             {
-                var token = GenerateJwtToken(user);
-
                 return Ok(new
                 {
-                    Token = token,
-                    Expiration = DateTime.UtcNow.AddDays(7)
+                    Token = result.Token,
+                    Expiration = result.Expiration
                 });
             }
 
-            return Unauthorized("Invalid email or password");
-        }
-        private string GenerateJwtToken(IdentityUser user)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.UTF8.GetBytes(_jwtSecretKey);
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new[]
-                {
-                new Claim(ClaimTypes.Name, user.UserName),
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.NameIdentifier, user.Id)
-            }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-
-            return tokenHandler.WriteToken(token);
+            return Unauthorized(result.ErrorMessage);
         }
     }
 }
